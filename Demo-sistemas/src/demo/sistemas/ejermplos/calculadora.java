84e8b303fbb5779f0e376cbc3fac29f4d237cfae
jorge Lopez <jr1552059@gmail.com>
2020-05-24 11:56:46 -0600

    segundo commit- cambios en clase calculadora

--- Demo-sistemas/src/demo/sistemas/ejermplos/calculadora.java
+++ Demo-sistemas/src/demo/sistemas/ejermplos/calculadora.java
@@ -6,10 +6,30 @@
 
 package demo.sistemas.ejermplos;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Jorge
  */
 public class calculadora {
     
+    
+    public static void main(String[] args) {
+        Scanner leer = new Scanner (System.in); 
+        
+        System.out.println("Intrusca el primer valor");
+        double x  = leer.nextDouble();
+        
+        System.out.println("Digite el segundo numero");
+        double y = leer.nextDouble();
+        System.out.println("La suma es:"+suma (x,y));
+        
+    }
+
+public static double suma(double a, double b){
+double sum= 0;
+sum = a + b;
+    return sum;
+}
 }
